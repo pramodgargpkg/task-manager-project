5378a8cc78cda51097181df1437a1650028c0fd8
pramodgarg <pramod.garg.pkg@gmail.com>
2022-04-03 14:26:37 +0530

    swagger configuration changed

--- src/main/java/com/pkgarg/taskmanager/Config/OpenApiConfig.java
+++ src/main/java/com/pkgarg/taskmanager/Config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.pkgarg.taskmanager.Config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
